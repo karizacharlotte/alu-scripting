a4f7a0f0d7a43c76e2f96d67b6c443b947c73fc0
karizacharlotte <c.kariza@alustudent.com>
2024-07-06 09:55:25 +0000

    x

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """
-This module provides a function to
-retrieve the number of subscribers for a given subreddit
-from Reddit's API
+function that queries the 'Reddit API'
+and prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
-    """
-    fetches the number of subscribers for a given subreddit
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=user_agent, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()['data']
-        if 'children' in data:
-            for post in data['children'][:10]:
-                print(post['data']['title'])
-    elif response.status_code == 404:
-        return None
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
